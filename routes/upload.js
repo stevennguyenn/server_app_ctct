50086c98c22af8c331c7ee306989fc5e6e5dcd64
vfa-ChauNN <vfa.chaunn@gmail.com>
2020-02-03 16:27:41 +0700

    change api upload image

--- routes/upload.js
+++ routes/upload.js
@@ -1,20 +1,17 @@
 const router = require("express").Router()
 const mongoose = require("mongoose")
 const Image = mongoose.model("Image")
-const upload = require("../middleware/upload")
+const fs = require('fs');
 
-router.post("/images", upload.single("image"), function(req, res) {
-    console.log(req.body)
-    console.log(req.file)
-    const image = new Image()
-    image.data = req.file.path
-    image.save(function(err, data) {
-        if(err) throw err
-        res.send({
-            status  : true,
-            message : "Successful",
-            data    : data
-        })
+router.post("/images", function(req, res) {
+    const {data, name} = req.body
+    const path = "images/" + name + Date.now() + ".jpg"
+    const base64Data = data.replace(/^data:([A-Za-z-+/]+);base64,/, '');
+    fs.writeFileSync(path, base64Data,  {encoding: 'base64'})
+    res.send({
+        status  : true,
+        message : "Successful",
+        data    : path
     })
 })
 
--- server.js
+++ server.js
@@ -19,6 +19,8 @@ app.use(bodyParser.json())
 app.use(morgan())
 
 //routes
+app.use(express.json({limit: '50mb'}))
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}))
 app.use("/users", require("./routes/users"))
 app.use("/theories", require("./routes/theories"))
 app.use("/upload", require("./routes/upload"))
