45685ce08c3b171c64a2cfe4ac52c7f23dca82fc
Steve <chaunguyen4297@gmail.com>
2020-06-13 11:11:46 +0700

    change api video

--- models/Course.js
+++ models/Course.js
@@ -5,13 +5,6 @@ const course_schema = mongoose.Schema({
         type: String,
         required: true
     },
-    list_video: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            required: true,
-            ref: "Video"
-        }
-    ],
     created_at: {
         type: Date,
         default: Date.now
